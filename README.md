19abad679b076660d45a4c4778273faa0dfbe373
andreaL14y <60392645+andreaL14y@users.noreply.github.com>
2020-03-09 10:20:18 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # LT2212 V20 Assignment 1
 
-Unfortunately the program I wrote is not compareable with the required structure as I defined the Dataframes of PART 1 and 3 globally and used its multiindex-structure for the plots. 
-As I don't know how strict the deadline is and I was not able to adjust everything in time I just uploaded this version and hope that I'll have the chance to formulate the tasks as function within the next days. 
+This program deals with the manipulation of textual data, especially within the structure of dataframes. 
 
-Considering the question of PART 4: 
-The tf-idf transformation of the Dataframe values squeezes down the value of very frequent used words such that the weight of words, which do not mediate any content but are naturally often used (such as articles 'a'/'the' or propositions 'in') are considered less "important" while rarer words get an higher weight. 
-This allows (more likely) to get the most important words considering the topics of the articles. 
+First some helper functions are implemented. 
+1. get_files to extract all files within two input folders
+2. get_words creates a list of words occuring in the input file
+3. get_word_couns takes a list of words and creates a dictionary containing a list of unique words and their number of occurances in the initial list
+
+#### Part 1: The function takes two folders and an integer as input and creates a dataframe. The multiindex consists of the article/file name of every file in the input folders and the folder where it was found. 
+For each word occuring at least once in any text a column is created. The integer value in cell C_ij derives the total occurence of word j in article i.
+
+#### Part 2: The 10 most used words (in total) of the dataframe from part 1 are taken. A bar chart is created that illustrates how often the each word is used within each of the input folders.
+
+#### Part 3: The values within the cells of the dataframe of Part 1 are transformed via the raw count version of term frequency. The function creates a new dataframe out of the dataframe from part 1.
+
+#### Part 4: As in part 2 the output of part 3 is illustrated. The tf-idf transformation of the Dataframe values squeezes down the value of very frequent used words such that the weight of words, which do not mediate any content but are naturally often used (such as articles 'a'/'the' or propositions 'in') are considered less "important" while rarer words get an higher weight. 
+This allows (more likely) to get the most important words considering the content of the articles. 
 As it can be seen in the second plot there is still appearing words like 'the' due to its very high fequency (however on 3rd not on first place) but also words like 'min', 'oil' or 'tonnes', that allow conclusions/suggestions about the topics of the articles. 
---
+++ a1.py
@@ -0,0 +1,105 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import numpy.random as npr
+
+# ADDED IMPORTS: 
+from os import walk
+from os.path import join
+import re
+import math 
+
+# "HELPER" FUNCTIONS:
+def get_files(folder1, folder2):
+    f = []
+    for (root, dirnames, _) in walk('.'): #if grain/crude are in same directory as code, otherwise change expression in 'walk'
+        for dirname in dirnames:
+            if dirname == folder1 or dirname == folder2:
+                subfolder = os.path.join(root, dirname)
+                for (_, _, filenames) in walk(subfolder):
+                    for filename in filenames:
+                        f.append(os.path.join(subfolder, filename))
+    return f
+
+#function to get all unique words used in an article, ignoring spelling with big letters 
+def get_words(path):
+    with open(path, 'r') as article:
+        content = article.read().lower()
+        words = re.findall(r'\w+', content) #as 'content.split()' would exclude words in brackets like "(word)"
+        filtered = [x for x in words if re.match('[a-z]+', x)] # alternative: x.isalpha
+        return filtered
+
+#function to count words
+def get_word_counts(words):
+    word_counts = {}
+    unique_words = set(words)
+    for word in unique_words:
+        word_counts[word] = words.count(word)
+    return word_counts
+
+#### PART 1:
+def part1_load(folder1, folder2, n=1):
+    all_files = get_files(folder1, folder2)
+    all_words=[]
+    all_subfolders=[]
+    all_files1=[]
+    for article in all_files:
+        words = get_words(article)
+        word_counts=get_word_counts(words)
+        all_words.append(word_counts)
+        all_subfolders.append(article.split('\\')[1])
+        all_files1.append(article.split('\\')[2].split('.')[0])
+
+    arrays = [np.asarray(all_files1), np.asarray(all_subfolders)]
+    idx = pd.MultiIndex.from_arrays(arrays, names=('article', 'folder')) # multiindex category 'folder' could be converted to a regular column: df = df.reset_index(level='folder')
+    df = pd.DataFrame(all_words, index=idx)
+    df = df.fillna(0) 
+    df_sum = df.sum() #global wordcount 
+
+    # delete columns of words that occur less than n times in df
+    for key, value in df_sum.items():
+        if value < n:
+            del df[key]
+    return df
+
+#### PART 2:
+def part2_vis(df):
+    # DO NOT CHANGE
+    assert isinstance(df, pd.DataFrame)
+
+    df_sum = df.sum() #global wordcount
+    df_folders = df.sum(level='folder') # wordcount per folder
+    df_top_n = df_sum.nlargest(n=10) #n most used words
+
+    word_index = []
+    crude = []
+    grain = []
+    for key, _ in df_top_n.items():
+        word_index.append(key)
+        crude.append(df_folders[key][0])
+        grain.append(df_folders[key][1])
+
+    df_plot = pd.DataFrame({'crude': crude, 'grain': grain}, index=word_index)
+    return df_plot.plot.bar(rot=0)
+
+
+#### PART 3:
+def part3_tfidf(df):
+    # DO NOT CHANGE
+    assert isinstance(df, pd.DataFrame)
+
+    df_tfidf = df.copy()
+    N = df_tfidf.shape[0] # total amount of documents, alternative: len(df.index)
+    K = df_tfidf.shape[1] # total amount of articles
+    d = (df > 0).sum(axis=0) # amount of articles that contain the word
+
+    k=0
+    while k < K: #iterates through every column in df
+        a = 0
+        while a < N: # iterates through every row in df
+            if df_tfidf.iat[a, k] != 0:
+                df_tfidf.iat[a, k] = df_tfidf.iat[a, k] * math.log(N/d[k]) #change value in row a, column k to tfidf value
+            a=a+1
+        k=k+1
+    return df_tfidf
